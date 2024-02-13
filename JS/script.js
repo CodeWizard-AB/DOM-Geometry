2f5a6ac313a8a1870e4ef59d50039a6d14a8b239
Anunay Argha <anunayargha@gmail.com>
2024-02-13 13:00:31 +0600

    project done

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,25 @@
 "use strict";
 
-document.querySelector(".geometry").addEventListener("click", function (e) {
-	const value1 = this.querySelector(".input-1").value;
-	const value2 = this.querySelector(".input-2").value;
-	if (e.target.classList.contains("btn") && value1 && value2) {
-		console.log(0.5 * value1 * value2);
-	}
+document.querySelectorAll(".geometry").forEach((card) => {
+	card.addEventListener("click", function (e) {
+		let area;
+		const value1 = this.querySelector(".input-1").value;
+		const value2 = this.querySelector(".input-2").value;
+		const figure = this.querySelector("img").getAttribute("alt");
+		if (e.target.classList.contains("btn") && value1 && value2) {
+			if (
+				figure === "triangle" ||
+				figure === "pentagon" ||
+				figure === "rhombus"
+			) {
+				area = 0.5 * value1 * value2;
+			} else if (figure === "ellipse") {
+				area = Math.PI * value1 * value2;
+			} else {
+				area = value1 * value2;
+			}
+			document.querySelector(".result").textContent = `${area.toFixed(2)} cm²`;
+			document.querySelectorAll("input").forEach((input) => (input.value = ""));
+		}
+	});
 });
